5023cd2493f4f8097db2cb12a545c7c9cecd5f89
James R. Lambert <ryolambert@gmail.com>
2019-05-21 20:03:14 -0600

    Feat(AuthorTemp): Added base file for author post list template

---
+++ src/templates/author.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { graphql } from 'gatsby';
+import GridContainer from 'components/Grid/GridContainer.jsx';
+import GridItem from 'components/Grid/GridItem';
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+
+
