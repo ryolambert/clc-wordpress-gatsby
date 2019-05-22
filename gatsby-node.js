c78e9e79a73ecad7389767dfb8566c6639deb801
James R. Lambert <ryolambert@gmail.com>
2019-05-21 20:04:27 -0600

    Feat(GB Node): Added GB node configuration for page templating

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,8 +5,71 @@
  */
 
 // You can delete this file if you're not using it
-const path = require(`path`);
-const { createFilePath } = require(`gatsby-source-filesystem`);
+
+const _ = require(`lodash`)
+const Promise = require(`bluebird`)
+const path = require(`path`)
+const slash = require(`slash`)
+const queryAll = require(`./src/queries/queryAll.js`)
+const createPaginatedPages = require("gatsby-paginate")
+
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions;
+
+    return new Promise((resolve, reject) => {
+        // Templates
+        const pageTemplate = path.resolve("./src/templates/page-template.js");
+        const postTemplate = path.resolve("./src/templates/post-template.js");
+
+        resolve(
+            graphql(queryAll).then(result => {
+                if (result.errors) reject(result.errors)
+
+                // Pages detail
+                const pages = result.data.allWordpressPage.edges
+
+                pages.forEach(edge => {
+                    createPage({
+                        path: `/${edge.node.slug}/`,
+                        component: slash(pageTemplate),
+                        context: {
+                            id: edge.node.id,
+                        },
+                    })
+                })
+
+                // Posts detail
+                const posts = result.data.allWordpressPost.edges
+
+                createPaginatedPages({
+                    edges: posts,
+                    createPage: createPage,
+                    pageTemplate: "src/templates/posts-template.js",
+                    pageLength: 3,
+                    pathPrefix: "posts"
+                })
+
+                posts.forEach(edge => {
+                    createPage({
+                        path: `/post/${edge.node.slug}/`,
+                        component: slash(postTemplate),
+                        context: {
+                            id: edge.node.id,
+                        },
+                    });
+                })
+
+            })
+        )
+    });
+};
+
+
+
+
+
+// const path = require(`path`);
+// const { createFilePath } = require(`gatsby-source-filesystem`);
 
 // exports.createPages = ({ graphql, actions }) => {
 //   const { createPage } = actions;
