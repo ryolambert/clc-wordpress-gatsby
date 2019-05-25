e0e382e14c88b66a5386a8848acb0062608f36a3
James R. Lambert <ryolambert@gmail.com>
2019-05-25 00:14:39 -0600

    Fix(Format): formatting quotes

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames';
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
+import withStyles from '@material-ui/core/styles/withStyles';
 
 // @material-ui/icons
 
@@ -10,20 +10,20 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import { FaPlay } from 'react-icons/fa';
 
 // core components
-import Header from "components/Header/Header.jsx";
-import Footer from "components/Footer/Footer.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
-import Button from "components/CustomButtons/Button.jsx";
-import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import Parallax from "components/Parallax/Parallax.jsx";
+import Header from 'components/Header/Header.jsx';
+import Footer from 'components/Footer/Footer.jsx';
+import GridContainer from 'components/Grid/GridContainer.jsx';
+import GridItem from 'components/Grid/GridItem.jsx';
+import Button from 'components/CustomButtons/Button.jsx';
+import HeaderLinks from 'components/Header/HeaderLinks.jsx';
+import Parallax from 'components/Parallax/Parallax.jsx';
 
-import landingPageStyle from "assets/jss/material-kit-react/views/landingPage.jsx";
+import landingPageStyle from 'assets/jss/material-kit-react/views/landingPage.jsx';
 
 // Sections for this page
-import ProductSection from "./Sections/ProductSection.jsx";
-import TeamSection from "./Sections/TeamSection.jsx";
-import WorkSection from "./Sections/WorkSection.jsx";
+import ProductSection from './Sections/ProductSection.jsx';
+import TeamSection from './Sections/TeamSection.jsx';
+import WorkSection from './Sections/WorkSection.jsx';
 
 const dashboardRoutes = [];
 
@@ -40,11 +40,11 @@ class LandingPage extends React.Component {
           fixed
           changeColorOnScroll={{
             height: 400,
-            color: "white"
+            color: 'white'
           }}
           {...rest}
         />
-        <Parallax filter image={require("assets/img/elysian-park.jpg")}>
+        <Parallax filter image={require('assets/img/elysian-park.jpg')}>
           <div className={classes.container}>
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
@@ -57,13 +57,12 @@ class LandingPage extends React.Component {
                 </h4>
                 <br />
                 <Button
-                  color="danger"
+                  color="warning"
                   size="lg"
                   href="https://www.youtube.com/watch?v=YfRlP2VEDEI"
                   target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaPlay/>
+                  rel="noopener noreferrer">
+                  <FaPlay />
                   Watch video
                 </Button>
               </GridItem>
