fb5cf14a2a9a294ed89e72e9ac0a56d9e12e6b66
James R. Lambert <ryolambert@gmail.com>
2019-05-26 12:07:16 -0600

    Fix(path): pathing on gatsby-browser.js for styling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,5 +6,5 @@
 
 // You can delete this file if you're not using it
 exports.onInitialClientRender = () => {
-  require("./src/assets/material-kit-react.scss");
+  require("./src/assets/scss/material-kit-react.scss");
 };
