c6a0bc2f9b00dc056d73efa6ac0ee9cc7b4010b0
James R. Lambert <ryolambert@gmail.com>
2019-05-26 14:05:01 -0600

    Fix(layout): removed div

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ import '../assets/scss/material-kit-react.scss';
 const Layout = ({ children }) => (
   <>
     <Header />
-    <div className="site-content">{children}</div>
+    {children}
     <Footer />
   </>
 );
