bbb445f3c5d40d05ebce5220be0ca4193b775902
James R. Lambert <ryolambert@gmail.com>
2019-05-26 14:40:35 -0600

    🙏

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,8 @@
 // );
 import React from 'react';
 import LandingPage from './LandingPage/LandingPage';
-import '../assets/scss/material-kit-react.scss';
+
+import "assets/scss/material-kit-react.scss?v=1.4.0";
 import 'typeface-roboto';
 import 'typeface-roboto-slab';
 
