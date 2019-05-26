d897053c7caf9e908fbb4f2d181ffd2a557e2183
James R. Lambert <ryolambert@gmail.com>
2019-05-26 12:37:52 -0600

    Fix(pathing): fixed scss pathing again

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import LandingPage from './LandingPage/LandingPage';
-import '../assets/scss/material-kit-react';
+import '../assets/scss/material-kit-react.scss';
 
 export default () => <LandingPage />;
