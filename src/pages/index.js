c815d79e8240754fac1dd1d0e2e5bcd9fc01211f
James R. Lambert <ryolambert@gmail.com>
2019-05-26 13:57:38 -0600

    Fix(page): just set index to the landing page view to see if that's what is causing our issue?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import { createMemoryHistory } from 'history';
-import { Route, Router, Switch } from 'react-router-dom';
+// import React from 'react';
+// import { createMemoryHistory } from 'history';
+// import { Route, Router, Switch } from 'react-router-dom';
 
+// import '../assets/scss/material-kit-react.scss';
+// import 'typeface-roboto';
+// import 'typeface-roboto-slab';
+
+// // pages for this product
+// import Components from './Components/Components.jsx';
+// import LandingPage from './LandingPage/LandingPage.jsx';
+// import ProfilePage from './ProfilePage/ProfilePage.jsx';
+// import LoginPage from './LoginPage/LoginPage.jsx';
+
+// let hist = createMemoryHistory();
+
+// export default () => (
+//   <Router history={hist}>
+//     <Switch>
+//       <Route path="/component-page" component={Components} />
+//       <Route path="/profile-page" component={ProfilePage} />
+//       <Route path="/login-page" component={LoginPage} />
+//       <Route path="/" component={LandingPage} />
+//     </Switch>
+//   </Router>
+// );
+import React from 'react';
+import LandingPage from './LandingPage/LandingPage';
 import '../assets/scss/material-kit-react.scss';
 import 'typeface-roboto';
 import 'typeface-roboto-slab';
 
-// pages for this product
-import Components from './Components/Components.jsx';
-import LandingPage from './LandingPage/LandingPage.jsx';
-import ProfilePage from './ProfilePage/ProfilePage.jsx';
-import LoginPage from './LoginPage/LoginPage.jsx';
-
-let hist = createMemoryHistory();
-
-export default () => (
-  <Router history={hist}>
-    <Switch>
-      <Route path="/component-page" component={Components} />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/" component={LandingPage} />
-    </Switch>
-  </Router>
-);
+export default () => <LandingPage />;
