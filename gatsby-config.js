4a608dbcdb2c0bd065cb0f44f67eb6624a4d9288
James R. Lambert <ryolambert@gmail.com>
2019-05-26 16:13:23 -0600

    testing removal of sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -136,8 +136,8 @@ module.exports = {
           return entities;
         }
       }
-    },
-    `gatsby-plugin-sitemap`
+    }
+    // `gatsby-plugin-sitemap`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
   ]
