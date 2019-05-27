341b007467602b18515dc94070e1f3c419661346
James R. Lambert <ryolambert@gmail.com>
2019-05-27 14:35:47 -0600

    Fix(page): Fixed basic pages for styling jsx import & will later include gql for editing

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -17,39 +17,32 @@ import GridItem from 'components/Grid/GridItem.jsx';
 import Button from 'components/CustomButtons/Button.jsx';
 import HeaderLinks from 'components/Header/HeaderLinks.jsx';
 import Parallax from 'components/Parallax/Parallax.jsx';
+import Layout from '../../components/Layout/Layout';
 
-import landingPageStyle from '../../assets/jss/material-kit-react/views/landingPage.jsx';
-import '../../assets/scss/material-kit-react.scss';
+import landingPageStyle from '../../assets/jss/material-kit-react/views/landingPageStyle.jsx';
+// import '../../assets/scss/material-kit-react.scss';
 
 // Sections for this page
 import ProductSection from './Sections/ProductSection.jsx';
 import TeamSection from './Sections/TeamSection.jsx';
 import WorkSection from './Sections/WorkSection.jsx';
 
+// graphql query access to modify page content
+import { graphql } from 'gatsby';
+
 const dashboardRoutes = [];
+// const LANDING_PAGE_QUERY = graphql
 
 class LandingPage extends React.Component {
   render() {
     const { classes, ...rest } = this.props;
     return (
-      <div>
-        <Header
-          color="transparent"
-          routes={dashboardRoutes}
-          brand=""
-          rightLinks={<HeaderLinks />}
-          fixed
-          changeColorOnScroll={{
-            height: 400,
-            color: 'white'
-          }}
-          {...rest}
-        />
+      <Layout>
         <Parallax filter image={require('assets/img/los-angeles-skyline.jpg')}>
           <div className={classes.container}>
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
-                <h1 className={classes.title}>Your Story Starts With Us.</h1>
+                <h1 className={classes.title}>Community Through Faith.</h1>
                 <h4>
                   Every landing page needs a small description after the big
                   bold title, that's why we added this text here. Add here all
@@ -77,8 +70,7 @@ class LandingPage extends React.Component {
             <WorkSection />
           </div>
         </div>
-        <Footer />
-      </div>
+      </Layout>
     );
   }
 }
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,43 +1,43 @@
-import React from "react";
+import React from 'react';
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
-import InputAdornment from "@material-ui/core/InputAdornment";
+import withStyles from '@material-ui/core/styles/withStyles';
+import InputAdornment from '@material-ui/core/InputAdornment';
 // @material-ui/icons
-import Email from "@material-ui/icons/Email";
-import People from "@material-ui/icons/People";
-import LockOutlined from "@material-ui/icons/LockOutlined";
+import Email from '@material-ui/icons/Email';
+import People from '@material-ui/icons/People';
+import LockOutlined from '@material-ui/icons/LockOutlined';
 // React icons
-import {FaFacebook, FaTwitter, FaGooglePlusG } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaGooglePlusG } from 'react-icons/fa';
 // core components
-import Header from "components/Header/Header.jsx";
-import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import Footer from "components/Footer/Footer.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
-import Button from "components/CustomButtons/Button.jsx";
-import Card from "components/Card/Card.jsx";
-import CardBody from "components/Card/CardBody.jsx";
-import CardHeader from "components/Card/CardHeader.jsx";
-import CardFooter from "components/Card/CardFooter.jsx";
-import CustomInput from "components/CustomInput/CustomInput.jsx";
+import Header from 'components/Header/Header.jsx';
+import HeaderLinks from 'components/Header/HeaderLinks.jsx';
+import Footer from 'components/Footer/Footer.jsx';
+import GridContainer from 'components/Grid/GridContainer.jsx';
+import GridItem from 'components/Grid/GridItem.jsx';
+import Button from 'components/CustomButtons/Button.jsx';
+import Card from 'components/Card/Card.jsx';
+import CardBody from 'components/Card/CardBody.jsx';
+import CardHeader from 'components/Card/CardHeader.jsx';
+import CardFooter from 'components/Card/CardFooter.jsx';
+import CustomInput from 'components/CustomInput/CustomInput.jsx';
 
-import loginPageStyle from "assets/jss/material-kit-react/views/loginPage.jsx";
+import loginPageStyle from 'assets/jss/material-kit-react/views/loginPageStyle.jsx';
 
-import image from "assets/img/bg7.jpg";
+import image from 'assets/img/bg7.jpg';
 
 class LoginPage extends React.Component {
   constructor(props) {
     super(props);
     // we use this to make the card to appear after the page has been rendered
     this.state = {
-      cardAnimaton: "cardHidden"
+      cardAnimaton: 'cardHidden'
     };
   }
   componentDidMount() {
     // we add a hidden class to the card and after 700 ms we delete it and the transition appears
     setTimeout(
       function() {
-        this.setState({ cardAnimaton: "" });
+        this.setState({ cardAnimaton: '' });
       }.bind(this),
       700
     );
@@ -56,11 +56,10 @@ class LoginPage extends React.Component {
         <div
           className={classes.pageHeader}
           style={{
-            backgroundImage: "url(" + image + ")",
-            backgroundSize: "cover",
-            backgroundPosition: "top center"
-          }}
-        >
+            backgroundImage: 'url(' + image + ')',
+            backgroundSize: 'cover',
+            backgroundPosition: 'top center'
+          }}>
           <div className={classes.container}>
             <GridContainer justify="center">
               <GridItem xs={12} sm={12} md={4}>
@@ -74,27 +73,24 @@ class LoginPage extends React.Component {
                           href="#pablo"
                           target="_blank"
                           color="transparent"
-                          onClick={e => e.preventDefault()}
-                        >
-                          <FaTwitter/>
+                          onClick={e => e.preventDefault()}>
+                          <FaTwitter />
                         </Button>
                         <Button
                           justIcon
                           href="#pablo"
                           target="_blank"
                           color="transparent"
-                          onClick={e => e.preventDefault()}
-                        >
-                          <FaFacebook/>
+                          onClick={e => e.preventDefault()}>
+                          <FaFacebook />
                         </Button>
                         <Button
                           justIcon
                           href="#pablo"
                           target="_blank"
                           color="transparent"
-                          onClick={e => e.preventDefault()}
-                        >
-                          <FaGooglePlusG/>
+                          onClick={e => e.preventDefault()}>
+                          <FaGooglePlusG />
                         </Button>
                       </div>
                     </CardHeader>
@@ -107,7 +103,7 @@ class LoginPage extends React.Component {
                           fullWidth: true
                         }}
                         inputProps={{
-                          type: "text",
+                          type: 'text',
                           endAdornment: (
                             <InputAdornment position="end">
                               <People className={classes.inputIconsColor} />
@@ -122,7 +118,7 @@ class LoginPage extends React.Component {
                           fullWidth: true
                         }}
                         inputProps={{
-                          type: "email",
+                          type: 'email',
                           endAdornment: (
                             <InputAdornment position="end">
                               <Email className={classes.inputIconsColor} />
@@ -137,10 +133,10 @@ class LoginPage extends React.Component {
                           fullWidth: true
                         }}
                         inputProps={{
-                          type: "password",
+                          type: 'password',
                           endAdornment: (
                             <InputAdornment position="end">
-                              <LockOutlined/>
+                              <LockOutlined />
                             </InputAdornment>
                           )
                         }}
