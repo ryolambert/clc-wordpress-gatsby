efbfb67aa72c66b985b2b13beb60889442a63fb0
James R. Lambert <ryolambert@gmail.com>
2019-05-27 19:09:27 -0600

    Fix(content): Changed content over to personal branding

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,15 @@
 /*eslint-disable*/
-import React from "react";
+import React from 'react';
 // nodejs library to set properties for components
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 // nodejs library that concatenates classes
-import classNames from "classnames";
-import { List, ListItem, withStyles } from "@material-ui/core";
+import classNames from 'classnames';
+import { List, ListItem, withStyles } from '@material-ui/core';
 
 // @material-ui/icons
-import Favorite from "@material-ui/icons/Favorite";
+import Favorite from '@material-ui/icons/Favorite';
 
-import footerStyle from "assets/jss/material-kit-react/components/footerStyle.jsx";
+import footerStyle from 'assets/jss/material-kit-react/components/footerStyle.jsx';
 
 function Footer({ ...props }) {
   const { classes, whiteFont } = props;
@@ -28,52 +28,47 @@ function Footer({ ...props }) {
           <List className={classes.list}>
             <ListItem className={classes.inlineBlock}>
               <a
-                href="https://www.creative-tim.com/"
+                href="https://www.relevantdesign.org/"
                 className={classes.block}
-                target="_blank"
-              >
-                Creative Tim
+                target="_blank">
+                Relevant Design
               </a>
             </ListItem>
             <ListItem className={classes.inlineBlock}>
               <a
-                href="https://www.creative-tim.com/presentation"
+                href="https://relevantdesign.org/#2"
                 className={classes.block}
-                target="_blank"
-              >
-                About us
+                target="_blank">
+                About
               </a>
             </ListItem>
-            <ListItem className={classes.inlineBlock}>
+            {/* <ListItem className={classes.inlineBlock}>
               <a
-                href="http://blog.creative-tim.com/"
+                href="http://blog.relevantdesign.org/"
                 className={classes.block}
-                target="_blank"
-              >
+                target="_blank">
                 Blog
               </a>
-            </ListItem>
-            <ListItem className={classes.inlineBlock}>
+            </ListItem> */}
+            {/* <ListItem className={classes.inlineBlock}>
               <a
-                href="https://www.creative-tim.com/license"
+                href="https://www.relevantdesign.org/licenses"
                 className={classes.block}
-                target="_blank"
-              >
+                target="_blank">
                 Licenses
               </a>
-            </ListItem>
+            </ListItem> */}
           </List>
         </div>
         <div className={classes.right}>
-          &copy; {1900 + new Date().getYear()} , made with{" "}
-          <Favorite className={classes.icon} /> by{" "}
+          &copy; {1900 + new Date().getYear()} , made with{' '}
+          <Favorite className={classes.icon} /> by{' '}
           <a
-            href="https://www.creative-tim.com"
+            href="https://relevantdesign.org"
             className={aClasses}
-            target="_blank"
-          >
-            Creative Tim
-          </a>{" "}
+            target="_blank">
+            Relevant Design
+          </a>{' '}
           for a better web.
         </div>
       </div>
