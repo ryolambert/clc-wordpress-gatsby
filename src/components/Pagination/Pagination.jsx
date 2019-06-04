51004ae87818108d7ba5ec84f3d00e9af3f9c136
James R. Lambert <ryolambert@gmail.com>
2019-06-03 20:33:22 -0600

    Fix(format): changed quotes

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 // nodejs library to set properties for components
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames';
 
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
-import Button from "@material-ui/core/Button";
+import withStyles from '@material-ui/core/styles/withStyles';
+import Button from '@material-ui/core/Button';
 
-import paginationStyle from "assets/jss/material-kit-react/components/paginationStyle.jsx";
+import paginationStyle from 'assets/jss/material-kit-react/components/paginationStyle.jsx';
 
 function Pagination({ ...props }) {
   const { classes, pages, color } = props;
@@ -29,8 +29,7 @@ function Pagination({ ...props }) {
             ) : (
               <Button
                 onClick={() => console.log("you've clicked " + prop.text)}
-                className={paginationLink}
-              >
+                className={paginationLink}>
                 {prop.text}
               </Button>
             )}
@@ -42,7 +41,7 @@ function Pagination({ ...props }) {
 }
 
 Pagination.defaultProps = {
-  color: "primary"
+  color: 'primary'
 };
 
 Pagination.propTypes = {
@@ -53,12 +52,12 @@ Pagination.propTypes = {
       disabled: PropTypes.bool,
       text: PropTypes.oneOfType([
         PropTypes.number,
-        PropTypes.oneOf(["PREV", "NEXT", "..."])
+        PropTypes.oneOf(['PREV', 'NEXT', '...'])
       ]).isRequired,
       onClick: PropTypes.func
     })
   ).isRequired,
-  color: PropTypes.oneOf(["primary", "info", "success", "warning", "danger"])
+  color: PropTypes.oneOf(['primary', 'info', 'success', 'warning', 'danger'])
 };
 
 export default withStyles(paginationStyle)(Pagination);
