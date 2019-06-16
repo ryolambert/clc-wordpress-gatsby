abcefa83e98a49bb901a1a81a2ab93819eca7c46
James R. Lambert <ryolambert@gmail.com>
2019-06-15 22:27:58 -0600

    Feat(posts): boiler for post, sermon, and event list pages

---
+++ src/components/Post/PostCategoriesNav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import SearchBarBlog from '../Search/SearchBarBlog';
+import postCategoriesNavStyle from '../../assets/jss/material-kit-react/components/postCategoriesNavStyle.jsx';
+
+const PostCategoriesNav = ({ categories, enableSearch }) => (
+  <div className="PostCategoriesNav">
+    <Link className="NavLink" exact="true" to={`/posts/`}>
+      All
+    </Link>
+    {categories.map((category, index) => (
+      <Link
+        exact="true"
+        className="NavLink"
+        key={category.title + index}
+        to={category.slug}>
+        {category.title}
+      </Link>
+    ))}
+
+    {enableSearch && <SearchBarBlog />}
+  </div>
+);
+
+export default PostCategoriesNav;
