3817744c7deffcd6588995f44869c93d0cb34e6f
James R. Lambert <ryolambert@gmail.com>
2019-08-12 16:02:30 -0600

    Feat(infoPanel): updated infoPanel from Info component & added stylesheet

---
+++ src/assets/jss/material-kit-react/components/infoPanelStyle.jsx
@@ -0,0 +1,64 @@
+import {
+  grayColor,
+  primaryColor,
+  infoColor,
+  successColor,
+  warningColor,
+  dangerColor
+} from 'assets/jss/material-kit-react.jsx';
+
+const infoPanelStyle = theme => ({
+  infoPanelContainer: {
+    height: 'auto',
+    overflow: 'hidden',
+    lineHeight: '0.5rem',
+    margin: '5px'
+  },
+  title: {
+    fontSize: '1.25rem',
+    lineHeight: '1rem',
+    margin: '5px 0px 5px',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '0.75rem',
+      linHeight: '0.4rem'
+    }
+  },
+  subtitle: {
+    fontSize: '1rem',
+    lineHeight: '0.75rem',
+    margin: '5px 0px 5px',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '0.5rem',
+      linHeight: '0.5rem'
+    }
+  },
+  directionsLink: {
+    color: '#fff',
+    textShadow: '0.05em 0.08em 0.2em rgba(0,0,0,.85)',
+    border: '0',
+    backgroundColor: warningColor,
+    transition: 'all .3s',
+    padding: '0px 11px',
+    marginBottom: '10px',
+    minWidth: '80%',
+    height: '30px',
+    minHeight: 'auto',
+    lineHeight: '30px',
+    fontWeight: '500',
+    fontSize: '14px',
+    textTransform: 'uppercase',
+    background: 'transparent',
+    textDecoration: 'none',
+    boxSizing: 'border-box',
+    '&:hover,&:focus': {
+      zIndex: '3',
+      backgroundColor: '#eee',
+      borderColor: '#ddd'
+    },
+    '&:hover': {
+      cursor: 'pointer'
+    }
+  }
+});
+
+export default infoPanelStyle;
---
+++ src/components/Map/InfoPanel.jsx
@@ -0,0 +1,101 @@
+import React, { PureComponent } from 'react';
+import Img from 'gatsby-image';
+// nodejs library that concatenates classes
+import classNames from 'classnames';
+// @material-ui/core components
+import Button from 'components/CustomButtons/Button.jsx';
+import withStyles from '@material-ui/core/styles/withStyles';
+// styling
+import infoPanelStyle from 'assets/jss/material-kit-react/components/infoPanelStyle';
+
+class InfoPanel extends PureComponent {
+  render() {
+    const { classes, info, position } = this.props;
+    const encodedQuery = encodeURIComponent(position);
+    const fluidContent = info.featured_media
+      ? info.featured_media.localFile.childImageSharp.fluid
+      : null;
+    console.log(info);
+    return (
+      <div className={classes.infoPanelContainer}>
+        <h6 style={{ margin: '0px' }}>
+          <strong
+            className={classes.title}
+            dangerouslySetInnerHTML={{
+              __html: info.acf.event_location
+                ? info.acf.event_location
+                : info.acf.landing_location
+                ? info.acf.landing_location
+                : null
+            }}
+          />
+        </h6>
+        <h3
+          className={classes.subtitle}
+          dangerouslySetInnerHTML={{
+            __html: info.acf.event_location_description
+              ? info.acf.event_location_description
+              : info.acf.landing_location_description
+              ? info.acf.landing_location_description
+              : null
+          }}
+        />
+        <p
+          style={{ fontSize: '0.5rem' }}
+          dangerouslySetInnerHTML={{
+            __html: info.acf.event_start ? info.acf.event_start : null
+          }}
+        />
+        <p
+          style={{ fontSize: '0.5rem' }}
+          dangerouslySetInnerHTML={{
+            __html: info.acf.event_end ? info.acf.event_end : null
+          }}
+        />
+        <p
+          style={{ fontSize: '0.5rem' }}
+          dangerouslySetInnerHTML={{
+            __html: info.acf.event_address
+              ? info.acf.event_address
+              : info.acf.landing_address
+              ? info.acf.landing_address
+              : null
+          }}
+        />
+        <br />
+        <div style={{ textAlign: 'center' }}>
+          <Button
+            size="sm"
+            round
+            style={{ textAlign: 'center' }}
+            className={classes.directionsLink}
+            color="warning">
+            <a
+              style={{ fontSize: '0.5rem', color: 'white' }}
+              target="_new"
+              href={`https://www.google.com/maps/dir/?api=1&destination=${encodedQuery}`}>
+              👉 Directions
+            </a>
+          </Button>
+        </div>
+        {fluidContent && (
+          <div>
+            <Img
+              style={{
+                width: 'auto',
+                maxWidth: '100%',
+                maxHeight: '5vh',
+                overflow: 'hidden',
+                borderRadius: '5px'
+              }}
+              alt="Screenshot of Project"
+              fluid={info.featured_media.localFile.childImageSharp.fluid}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default withStyles(infoPanelStyle)(InfoPanel);
