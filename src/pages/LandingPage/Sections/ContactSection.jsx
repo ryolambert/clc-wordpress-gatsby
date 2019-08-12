e3b7f92a962b365ce1709788cf9eab0320e3bdde
James R. Lambert <ryolambert@gmail.com>
2019-08-12 16:06:46 -0600

    Fix(contact): updated contact section

--- src/pages/LandingPage/Sections/ContactSection.jsx
+++ src/pages/LandingPage/Sections/ContactSection.jsx
@@ -7,6 +7,7 @@ import GridItem from 'components/Grid/GridItem.jsx';
 
 const ContactSection = props => {
   const { classes, pageInfo, address, ...rest } = props;
+  const infoPanel = pageInfo;
   return (
     <div>
       <GridContainer justify="center">
@@ -14,11 +15,7 @@ const ContactSection = props => {
           <ContactForm />
         </GridItem>
         <GridItem xs={12} sm={12} md={6}>
-          <Map
-            className="leaflet-container"
-            position={address}
-            info={pageInfo}
-          />
+          <Map position={address} info={pageInfo} />
         </GridItem>
       </GridContainer>
     </div>
