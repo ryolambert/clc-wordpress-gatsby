dbe0de07a6321da07581c0d490b4eda30493a6aa
Ryo Lambert <ryolambert@gmail.com>
2019-08-14 01:02:49 +0100

    Fix(refactor): took out unecessary components page route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createMemoryHistory } from 'history';
 import { Router, Link } from '@reach/router';
-import Components from './Components/Components.jsx';
 import LandingPage from './LandingPage/LandingPage.jsx';
 
 
@@ -10,6 +9,5 @@ let hist = createMemoryHistory();
 export default () => (
   <Router>
     <LandingPage path="/" />
-    <Components path="/component-page" />
   </Router>
 );
