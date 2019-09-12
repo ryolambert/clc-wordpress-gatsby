c338f7612c99f5615ef36196bd8d296e81a844ae
Ryo Lambert <ryolambert@gmail.com>
2019-09-13 00:47:29 +0100

    ✅Lint/Debug: Add/fix settings

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+  parser: 'babel',
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   semi: true,
-  singleQuote: true,
   trailingComma: 'es5',
+  singleQuote: true,
   bracketSpacing: true,
   jsxBracketSameLine: false,
   proseWrap: 'always',
