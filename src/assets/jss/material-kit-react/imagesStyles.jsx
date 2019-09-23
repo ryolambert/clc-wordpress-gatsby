a697833904016505c3d546e2175fbf928aaf30da
Ryo Lambert <ryolambert@gmail.com>
2019-09-23 20:09:36 +0100

    👍UPDATE: add ImgCenter CSS class

--- src/assets/jss/material-kit-react/imagesStyles.jsx
+++ src/assets/jss/material-kit-react/imagesStyles.jsx
@@ -3,21 +3,29 @@ const imagesStyles = {
     width: 'auto',
     maxWidth: '100%',
     height: 'auto',
-    maxHeight: '300px'
+    maxHeight: '300px',
+  },
+  imgCentered: {
+    width: 'auto',
+    maxWidth: '300px',
+    height: 'auto',
+    maxHeight: '300px',
+    overflow: 'hidden',
+    margin: '0 auto',
   },
   imgRounded: {
-    borderRadius: '6px !important'
+    borderRadius: '6px !important',
   },
   imgRoundedCircle: {
-    borderRadius: '50% !important'
+    borderRadius: '50% !important',
   },
   imgRaised: {
     boxShadow:
-      '0 5px 15px -8px rgba(0, 0, 0, 0.24), 0 8px 10px -5px rgba(0, 0, 0, 0.2)'
+      '0 5px 15px -8px rgba(0, 0, 0, 0.24), 0 8px 10px -5px rgba(0, 0, 0, 0.2)',
   },
   imgGallery: {
     width: '100%',
-    marginBottom: '2.142rem'
+    marginBottom: '2.142rem',
   },
   imgCardTop: {
     // height: 'auto',
@@ -26,16 +34,16 @@ const imagesStyles = {
     // maxHeight: '100%',
     width: '100%',
     borderTopLeftRadius: 'calc(.25rem - 1px)',
-    borderTopRightRadius: 'calc(.25rem - 1px)'
+    borderTopRightRadius: 'calc(.25rem - 1px)',
   },
   imgCardBottom: {
     width: '100%',
     borderBottomLeftRadius: 'calc(.25rem - 1px)',
-    borderBottomRightRadius: 'calc(.25rem - 1px)'
+    borderBottomRightRadius: 'calc(.25rem - 1px)',
   },
   imgCard: {
     width: '100%',
-    borderRadius: 'calc(.25rem - 1px)'
+    borderRadius: 'calc(.25rem - 1px)',
   },
   imgCardOverlay: {
     position: 'absolute',
@@ -43,13 +51,13 @@ const imagesStyles = {
     right: '0',
     bottom: '0',
     left: '0',
-    padding: '1.25rem'
+    padding: '1.25rem',
   },
   imgWrapper: {
     width: '843px',
     height: '180px',
-    overflow: 'hidden'
-  }
+    overflow: 'hidden',
+  },
 };
 
 export default imagesStyles;
